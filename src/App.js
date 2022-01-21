cc315e997b5dee01002fe438a3402698827db870
JJKEB <jjkeb47@gmail.com>
2022-01-21 15:42:39 +0900

    create components & markup styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import Todotemplate from './components/TodoTemplate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Todotemplate>
+        <TodoInsert />
+        <TodoList />
+      </Todotemplate>
     </div>
   );
 }
